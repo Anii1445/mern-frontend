94f39af9502e8e36c8260720ea41f6389eac20a2
Ankit Gupta <ankitgupta14998@users.noreply.github.com>
2026-02-12 13:51:15 +0530

    Update Frontend

--- src/pages/Product-View.jsx
+++ src/pages/Product-View.jsx
@@ -728,6 +728,11 @@ const handleActionClick = (action) => {
 
 return (
     <>
+     {isLoading ? <div  className="d-flex justify-content-center align-items-center" style={{ minHeight: "clamp(300px, 70vh, 800px)" }}>
+                  <div className="spinner-grow text-secondary" role="status">
+    </div>
+    <div className="text-muted">Loading...</div>
+                </div>:
       <div className="container">
 
          {/* //------------- Breadcrumbs ------------- // */}
@@ -1726,7 +1731,7 @@ sx={{
     </Button>
   </div>)}
 
-      </div>
+      </div>}
     </>
   );
 }
